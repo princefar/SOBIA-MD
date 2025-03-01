bcd8fb2422410bea419b5026ef5a3ec4ffd38a28
princefar <princetypist45@gmail.com>
2025-03-02 03:57:29 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "FGF0yZSB#K78EHmWei0p0EVmAabrqRNfoRAbPHIlgVMjFs1pTrkw",
     CAPTION: process.env.CAPTION || "POWERED BY SOBIA BUTT",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
